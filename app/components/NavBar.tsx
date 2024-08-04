ea8a33cb6de2a71aafecb516f1bef63fa0f3f422
Whit Allee <whitallee@gmail.com>
2024-08-04 14:49:06 -0500

    fixed apostrophe

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -9,7 +9,7 @@ export default function NavBar() {
     return(
         <div className="fixed top-0 overflow-hidden w-screen">
             <div className="flex w-full pt-4 pb-2 px-5 justify-between bg-black">
-                <h1>Whit's Pantry</h1>
+                <h1>Whit&apos;s Pantry</h1>
                 <div className="flex gap-2">
                     <Link href={"/"} className={path == "/" ? "opacity-20" : ""}><FormatListBulleted/></Link>
                     <Link href={"/photo-pantry"} className={path == "/photo-pantry" ? "opacity-20" : ""}><Inventory/></Link>
